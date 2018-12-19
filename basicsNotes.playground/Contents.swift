0e39be5f84456b3611227f4f7237d82e48d85c72
West Kraemer <kraemerwest@gmail.com>
2018-12-19 12:34:13 +0100

    add coordinates checking.

--- basicsNotes.playground/Contents.swift
+++ basicsNotes.playground/Contents.swift
@@ -1,86 +1,100 @@
-import UIKit
-import Foundation
+//import UIKit
+//import Foundation
+//
+//var str = "Hello, playground"
+//
+//var a = 11
+//var b = 22
+//
+//if a > b {
+//    print("\(a)")
+//} else { b > a
+//    print("\(b)")
+//}
+//
+//let number = 5
+//
+//if number % 2 == 0 {
+//    print("It's an even number")
+//} else {
+//    print("it's odd")
+//}
+//
+//var z = 99
+//var x = 50
+//
+//if z % x == 0 {
+//    print("divisible")
+//} else {
+//    print("not divisible")
+//}
+//
+//var n = 2
+//var y = 3
+//var c = 2
+//
+//if n == y || n == c || y == c {
+//    print("At least two variables have the same value")
+//} else {
+//    print("All the values are different")
+//}
+//
+//
+//var baconAge = 6 // the bacon is 6 days old
+//var eggsAge = 12 // eggs are 12 days old
+//
+//if (baconAge <= 7) && (eggsAge <= 21) {
+//    print("you can cook bacon and eggs")
+//}
+//else {
+//    if baconAge > 7 {
+//        print("throw out bacon")
+//    }
+//    if eggsAge > 21 {
+//        print("throw out eggs")
+//    }
+//}
+//
+//var randomNumber = arc4random()
+//
+//if randomNumber % 2 == 0 {
+//    print("heads")
+//    print("\(randomNumber)")
+//} else {
+//    print("tails")
+//    print("\(randomNumber)")
+//}
+//
+//var randomNumber2 = Float.random(in: 0...100)
+//
+////var randomNumber2 = 0
+//
+//if randomNumber2 >= 50 {
+//    print("heads")
+//    print("\(randomNumber2)")
+//} else {
+//    print("tails")
+//    print("\(randomNumber2)")
+//}
+//
+//var num = 21
+//
+//if num % 3 == 0 && num % 5 == 0 && num % 7 == 0 {
+//    print ("number is divisible by 3, 5 and 7")
+//} else {
+//    print("number is not divisible by 3, 5 and 7")
+//}
 
-var str = "Hello, playground"
 
-var a = 11
-var b = 22
+var x = 1
+var y = 2
+var lowX = 1
+var lowY = 1
+var highX = 3
+var highY = 3
 
-if a > b {
-    print("\(a)")
-} else { b > a
-    print("\(b)")
-}
-
-let number = 5
-
-if number % 2 == 0 {
-    print("It's an even number")
+if ((x <= highX) && (x >= lowX)) && ((y <= highY) && (y >= lowY)){
+    print("inside")
 } else {
-    print("it's odd")
-}
-
-var z = 99
-var x = 50
-
-if z % x == 0 {
-    print("divisible")
-} else {
-    print("not divisible")
-}
-
-var n = 2
-var y = 3
-var c = 2
-
-if n == y || n == c || y == c {
-    print("At least two variables have the same value")
-} else {
-    print("All the values are different")
-}
-
-
-var baconAge = 6 // the bacon is 6 days old
-var eggsAge = 12 // eggs are 12 days old
-
-if (baconAge <= 7) && (eggsAge <= 21) {
-    print("you can cook bacon and eggs")
-}
-else {
-    if baconAge > 7 {
-        print("throw out bacon")
-    }
-    if eggsAge > 21 {
-        print("throw out eggs")
-    }
-}
-
-var randomNumber = arc4random()
-
-if randomNumber % 2 == 0 {
-    print("heads")
-    print("\(randomNumber)")
-} else {
-    print("tails")
-    print("\(randomNumber)")
-}
-
-var randomNumber2 = Float.random(in: 0...100)
-
-//var randomNumber2 = 0
-
-if randomNumber2 >= 50 {
-    print("heads")
-    print("\(randomNumber2)")
-} else {
-    print("tails")
-    print("\(randomNumber2)")
-}
-
-var num = 21
-
-if num % 3 == 0 && num % 5 == 0 && num % 7 == 0 {
-    print ("number is divisible by 3, 5 and 7")
-} else {
-    print("number is not divisible by 3, 5 and 7")
+    print("not inside")
 }
