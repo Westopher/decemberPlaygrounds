3ea3a65ae42a20327429daf6e2889d3c38dc5385
West Kraemer <kraemerwest@gmail.com>
2018-12-23 10:11:27 +0100

    more for in loops

--- basicsNotes.playground/Contents.swift
+++ basicsNotes.playground/Contents.swift
@@ -1,108 +1,154 @@
-//import UIKit
-//import Foundation
+////import UIKit
+////import Foundation
+////
+////var str = "Hello, playground"
+////
+////var a = 11
+////var b = 22
+////
+////if a > b {
+////    print("\(a)")
+////} else { b > a
+////    print("\(b)")
+////}
+////
+////let number = 5
+////
+////if number % 2 == 0 {
+////    print("It's an even number")
+////} else {
+////    print("it's odd")
+////}
+////
+////var z = 99
+////var x = 50
+////
+////if z % x == 0 {
+////    print("divisible")
+////} else {
+////    print("not divisible")
+////}
+////
+////var n = 2
+////var y = 3
+////var c = 2
+////
+////if n == y || n == c || y == c {
+////    print("At least two variables have the same value")
+////} else {
+////    print("All the values are different")
+////}
+////
+////
+////var baconAge = 6 // the bacon is 6 days old
+////var eggsAge = 12 // eggs are 12 days old
+////
+////if (baconAge <= 7) && (eggsAge <= 21) {
+////    print("you can cook bacon and eggs")
+////}
+////else {
+////    if baconAge > 7 {
+////        print("throw out bacon")
+////    }
+////    if eggsAge > 21 {
+////        print("throw out eggs")
+////    }
+////}
+////
+////var randomNumber = arc4random()
+////
+////if randomNumber % 2 == 0 {
+////    print("heads")
+////    print("\(randomNumber)")
+////} else {
+////    print("tails")
+////    print("\(randomNumber)")
+////}
+////
+////var randomNumber2 = Float.random(in: 0...100)
+////
+//////var randomNumber2 = 0
+////
+////if randomNumber2 >= 50 {
+////    print("heads")
+////    print("\(randomNumber2)")
+////} else {
+////    print("tails")
+////    print("\(randomNumber2)")
+////}
+////
+////var num = 21
+////
+////if num % 3 == 0 && num % 5 == 0 && num % 7 == 0 {
+////    print ("number is divisible by 3, 5 and 7")
+////} else {
+////    print("number is not divisible by 3, 5 and 7")
+////}
 //
-//var str = "Hello, playground"
 //
-//var a = 11
-//var b = 22
+//var x = 1
+//var y = 2
+//var lowX = 1
+//var lowY = 1
+//var highX = 3
+//var highY = 3
 //
-//if a > b {
-//    print("\(a)")
-//} else { b > a
-//    print("\(b)")
-//}
-//
-//let number = 5
-//
-//if number % 2 == 0 {
-//    print("It's an even number")
+//if ((x <= highX) && (x >= lowX)) && ((y <= highY) && (y >= lowY)){
+//    print("inside")
 //} else {
-//    print("it's odd")
+//    print("not inside")
 //}
 //
-//var z = 99
-//var x = 50
+////arrays
+////
+////var listOfNumbers = [1, 2, 3, 10, 100]
 //
-//if z % x == 0 {
-//    print("divisible")
-//} else {
-//    print("not divisible")
+////var maxVal = listOfNumbers[0]
+////
+////for number in listOfNumbers {
+////    if maxVal < number {
+////        maxVal = number
+////    }
+////}
+////
+////print(maxVal)
+//
+////for number in listOfNumbers {
+////    if number % 2 != 0 {
+////        print(number)
+////    }
+////}
+//
+//var sum = 0
+//
+//for number in listOfNumbers {
+//    sum += number
 //}
 //
-//var n = 2
-//var y = 3
-//var c = 2
+//print(sum)
+////for each element in the array list of numbers, add that number to the sum, then print the total.
 //
-//if n == y || n == c || y == c {
-//    print("At least two variables have the same value")
-//} else {
-//    print("All the values are different")
+//
+////to do: finish with loops, then come back to arrays
+//
+//var N = 3
+//
+//for i in 1...N {
+//    print("I will not skip the fundamentals")
 //}
 //
-//
-//var baconAge = 6 // the bacon is 6 days old
-//var eggsAge = 12 // eggs are 12 days old
-//
-//if (baconAge <= 7) && (eggsAge <= 21) {
-//    print("you can cook bacon and eggs")
-//}
-//else {
-//    if baconAge > 7 {
-//        print("throw out bacon")
-//    }
-//    if eggsAge > 21 {
-//        print("throw out eggs")
-//    }
+//var daysDictionary = [1 : "SunDay", 2 : "MonDay", 3 : "TuesDay", 4 : "WednesDay", 5 : "ThursDay", 6 : "FriDay", 7 : "SaturDay"]
+//for (id, day) in daysDictionary {
+//    print ("\(id) , \(day)")
 //}
 //
-//var randomNumber = arc4random()
+//var listOfNumbers = [1, 2, 3, 10, 100]
 //
-//if randomNumber % 2 == 0 {
-//    print("heads")
-//    print("\(randomNumber)")
-//} else {
-//    print("tails")
-//    print("\(randomNumber)")
-//}
+//print("\(listOfNumbers.max()!)")
 //
-//var randomNumber2 = Float.random(in: 0...100)
 //
-////var randomNumber2 = 0
+//var listOfNumbers = [1, 2, 3, 10, 100]
 //
-//if randomNumber2 >= 50 {
-//    print("heads")
-//    print("\(randomNumber2)")
-//} else {
-//    print("tails")
-//    print("\(randomNumber2)")
-//}
-//
-//var num = 21
-//
-//if num % 3 == 0 && num % 5 == 0 && num % 7 == 0 {
-//    print ("number is divisible by 3, 5 and 7")
-//} else {
-//    print("number is not divisible by 3, 5 and 7")
-//}
-
-
-var x = 1
-var y = 2
-var lowX = 1
-var lowY = 1
-var highX = 3
-var highY = 3
-
-if ((x <= highX) && (x >= lowX)) && ((y <= highY) && (y >= lowY)){
-    print("inside")
-} else {
-    print("not inside")
-}
-
-//arrays
-
-var listOfNumbers = [1, 2, 3, 10, 100]
-
 //var maxVal = listOfNumbers[0]
 //
 //for number in listOfNumbers {
@@ -113,31 +159,11 @@ var listOfNumbers = [1, 2, 3, 10, 100]
 //
 //print(maxVal)
 
-//for number in listOfNumbers {
-//    if number % 2 != 0 {
-//        print(number)
-//    }
-//}
 
-var sum = 0
+var listOfNumbers = [1, 2, 3, 10, 100]
 
-for number in listOfNumbers {
-    sum += number
-}
-
-print(sum)
-//for each element in the array list of numbers, add that number to the sum, then print the total.
-
-
-//to do: finish with loops, then come back to arrays
-
-var N = 3
-
-for i in 1...N {
-    print("I will not skip the fundamentals")
-}
-
-var daysDictionary = [1 : "SunDay", 2 : "MonDay", 3 : "TuesDay", 4 : "WednesDay", 5 : "ThursDay", 6 : "FriDay", 7 : "SaturDay"]
-for (id,day) in daysDictionary{
-    print ("\(id) , \(day)")
+for num in listOfNumbers {
+    if num % 2 != 0 {
+        print(num)
+    }
 }
